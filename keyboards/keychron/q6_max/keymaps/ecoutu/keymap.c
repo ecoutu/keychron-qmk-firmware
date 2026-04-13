5f5519165327add03ceb46d1430065942dcdd10b
Emma Coutu <emma@coutu.io>
2026-04-12 19:41:06 -0600

    feat: replace tapdance with layer based macros
    Signed-off-by: Emma Coutu <emma@coutu.io>

--- keyboards/keychron/q6_max/keymaps/ecoutu/keymap.c
+++ keyboards/keychron/q6_max/keymaps/ecoutu/keymap.c
@@ -17,7 +17,8 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 #include "layers.h"
-#include "tap_dance.h"
+
+enum custom_keycodes { KC_W_HOLD = SAFE_RANGE, KC_Q_HOLD };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,14 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_109_ansi(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_SCRL,  KC_NO,    _______,  _______,  _______,  _______,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   TD(TD_Q), TD(TD_W), KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
-        TG(3),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
+        TG(3),    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                LT(3, KC_SPC),                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
     [WIN_FN] = LAYOUT_109_ansi(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_NO,      _______,  _______,  KC_NO,    _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,    _______,    _______,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  KC_Q_HOLD,  KC_W_HOLD,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,    _______,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  _______,  _______,  _______,
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______)
@@ -54,16 +55,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [MAC_FN]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [WIN_FN]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+  [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+  [MAC_FN] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+  [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+  [WIN_FN] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif // ENCODER_MAP_ENABLE
 
+static bool w_held = false;
+static bool q_held = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_keychron_common(keycode, record)) {
-        return false;
+  if (!process_record_keychron_common(keycode, record)) {
+    return false;
+  }
+  switch (keycode) {
+  case KC_W_HOLD:
+    if (record->event.pressed && !w_held) {
+      register_code(KC_W);
+      w_held = true;
     }
+    return false;
+  case KC_W:
+    w_held = false;
     return true;
+  case KC_Q_HOLD:
+    if (record->event.pressed && !q_held) {
+      register_code(KC_Q);
+      q_held = true;
+    }
+    return false;
+  case KC_Q:
+    q_held = false;
+    return true;
+  default:
+    break;
+  }
+  return true;
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+  if (!q_held && !w_held)
+    return true;
+  for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+    for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+      uint8_t led_i = g_led_config.matrix_co[row][col];
+      if (led_i == NO_LED || led_i < led_min || led_i >= led_max)
+        continue;
+      keypos_t pos = {.row = row, .col = col};
+      uint16_t keycode = keymap_key_to_keycode(WIN_BASE, pos);
+      if ((w_held && (keycode == KC_W_HOLD || keycode == KC_W)) || (q_held && (keycode == KC_Q_HOLD || keycode == KC_Q))) {
+        rgb_matrix_set_color(led_i, 0, 255, 0);
+      }
+    }
+  }
+  return true;
 }
--- keyboards/keychron/q6_max/keymaps/ecoutu/rules.mk
+++ keyboards/keychron/q6_max/keymaps/ecoutu/rules.mk
@@ -1,13 +1,13 @@
-# Via live remapping support
+#Via live remapping support
 VIA_ENABLE = yes
 
-# Tap dance support
-TAP_DANCE_ENABLE = yes
+#Tap dance support
+TAP_DANCE_ENABLE = no
 
-# Encoder mapping support
+#Encoder mapping support
 ENCODER_MAP_ENABLE = yes
 
-# RGB Matrix enabled - Keychron wireless_playground fork has the required SPI drivers
+#RGB Matrix enabled - Keychron wireless_playground fork has the required SPI drivers
 RGB_MATRIX_ENABLE = yes
+SRC += rgb.c
 
-SRC += tap_dance.c rgb.c
